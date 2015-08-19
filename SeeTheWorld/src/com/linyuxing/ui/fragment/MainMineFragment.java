0a3ae7132e3e777cbb8f2408c9c1ee573914d76f
kevin <kevin@kevin-PC>
2015-08-19 15:26:00 +0800

    test

--- SeeTheWorld/src/com/linyuxing/ui/fragment/MainMineFragment.java
+++ SeeTheWorld/src/com/linyuxing/ui/fragment/MainMineFragment.java
@@ -13,7 +13,6 @@ public class MainMineFragment extends Fragment{
 	private View mView;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		mView=inflater.inflate(R.layout.main_mine_fg,null);
 		return mView;
 	}
